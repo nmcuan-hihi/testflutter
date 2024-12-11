66487ffeeccfd0ce3b21b2c1769a5700c5a4e1df
ThanhHai2911 <hailedvs2@gmail.com>
2024-12-11 09:53:27 +0700

    Bottomnavigaton

---
+++ lib/presentation/booking.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'homePage.dart';
+import 'tuft.dart';
+import 'favorites.dart';
+import 'profile.dart';
+
+
+class Booking extends StatelessWidget {
+  const Booking({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Booking"),
+      ),
+
+    );
+  }
+}
+
+
+
---
+++ lib/presentation/profile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'homePage.dart';
+import 'tuft.dart';
+import 'favorites.dart';
+import 'booking.dart';
+
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      
+    );
+  }
+}
